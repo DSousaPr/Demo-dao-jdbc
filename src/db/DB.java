2613c950d204fd998e2a0332a17019ac1e981cd9
Djair Sousa <sousaprm99@gmail.com>
2022-05-05 02:01:12 -0300

    Creating project and git repository

---
+++ src/db/DB.java
@@ -0,0 +1,73 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class DB {
+
+	//método para conectar com banco de dados
+	private static Connection conn = null;
+	
+	//método para conectar AO banco de dados
+	public static Connection getConnection() {
+		if (conn == null) {
+			try {
+				Properties props = loadProperties();
+				String url = props.getProperty("dburl");
+				conn = DriverManager.getConnection(url, props);
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+		return conn;
+	}
+	
+	//Método para fechar a conexão com o banco de dados
+	public static void closeConection() {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	
+	//Metodo para carregar as propiedades que estão definidas no meu arquivo db.properties
+	private static Properties loadProperties () {
+		try (FileInputStream fs = new FileInputStream("db.properties")){
+			Properties props = new Properties();
+			props.load(fs);
+			return props;
+		} catch (IOException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+	
+	//método auxiliar para fechar os objetos na classe principal (Statement e ResultSet)
+	public static void closeStatement (Statement st) {
+		if (st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	
+	public static void closeResultSet (ResultSet rs ) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+}
